b965d186cff19684edd701a99adc8c04083a983e
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-07-07 12:44:52 +0200

    Update 1-x.R

--- test/evaluation/1-x.R
+++ test/evaluation/1-x.R
@@ -3,9 +3,6 @@ context({
     testEqual("length(aantal_doden)", function(env) { length(env$aantal_doden) }, 2)
     testEqual("length(aantal_gereden_km)", function(env) { length(env$aantal_gereden_km) }, 2)
   })
-})
-
-context({
   testcase('Risico is correct berekend:', {
     testEqual("risk", function(env) { (env$aantal_doden / env$aantal_gereden_km) * 10^8 }, risk)
   })
