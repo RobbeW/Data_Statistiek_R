3e4213bb03f06d35ee3e05b10a5badf29965f97d
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-07-07 12:52:26 +0200

    Update 1-x.R

--- test/evaluation/1-x.R
+++ test/evaluation/1-x.R
@@ -4,7 +4,7 @@ context({
     testEqual("length(aantal_gereden_km)", function(env) { length(env$aantal_gereden_km) }, 2)
   })
   testcase('Risico is correct berekend:', {
-    testEqual("risico[1]", function(env) { (env$aantal_doden[1] / env$aantal_gereden_km[1]) * 10^6 }, risico[1])
-    testEqual("risico[2]", function(env) { (env$aantal_doden[2] / env$aantal_gereden_km[2]) * 10^6 }, risico[2])
+    testEqual("risico", function(env) { (env$aantal_doden[1] / env$aantal_gereden_km[1]) * 10^6 }, risico)
+    testEqual("risico", function(env) { (env$aantal_doden[2] / env$aantal_gereden_km[2]) * 10^6 }, risico)
   })
 })
