9d86899b0d1fd93401173d2aa16a8484ae0115d0
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-08-07 16:33:07 +0200

    Update 2-x.R

--- Van afval tot grafieken/evaluation/2-x.R
+++ Van afval tot grafieken/evaluation/2-x.R
@@ -5,9 +5,6 @@ context({
     testEqual("length(percentage)", function(env) { length(env$percentage) }, 9)
   })
   testcase('Totaal gewicht is correct berekend:', {
-    testTrue("totale_gewicht is correct", function(env) {
-      computed_value <- env$totale_gewicht
-      computed_value == 231.9 || computed_value == 231.8 || computed_value == 232
-    })
+    testEqual("totale_gewicht", function(env) { sum(env$gewicht) }, 231.8)
   })
 })
