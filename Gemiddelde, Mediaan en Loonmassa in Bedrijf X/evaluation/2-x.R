811b02e37ecbd863324ed2f95b46348cae74a3b6
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-08-15 15:28:58 +0200

    Update 2-x.R

--- Gemiddelde, Mediaan en Loonmassa in Bedrijf X/evaluation/2-x.R
+++ Gemiddelde, Mediaan en Loonmassa in Bedrijf X/evaluation/2-x.R
@@ -1,8 +1,8 @@
 context({
   testcase('Controleer de variabele met de lonen:', {
     testEqual("length(salaries)", function(env) { length(env$salaries) }, 15)
-    testEqual("sum(salaries)", function(env) { sum(env$salaries) }, 56400)
-    testEqual("mean(salaries)", function(env) { mean(env$salaries) }, 3760)
+    testEqual("sum(salaries)", function(env) { sum(env$salaries) }, 64400)
+    testEqual("mean(salaries)", function(env) { mean(env$salaries) }, 4293.33333333333)
     testEqual("median(salaries)", function(env) { median(env$salaries) }, 4100)
   })
 })
