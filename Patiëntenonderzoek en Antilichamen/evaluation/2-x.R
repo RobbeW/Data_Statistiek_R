c5489df0b31701a77cfcab315e63283d06326b15
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-08-18 14:50:50 +0200

    Update 2-x.R

--- Patiëntenonderzoek en Antilichamen/evaluation/2-x.R
+++ Patiëntenonderzoek en Antilichamen/evaluation/2-x.R
@@ -1,6 +1,6 @@
 context({
-  testcase('Check if patient_names variable is created correctly', {
-    testEqual("length(patient_namen)", function(env) { length(env$patient_names) }, 5)
+  testcase('Controleer de lengte van de vector bij variabele -namen-', {
+    testEqual("length(patient_namen)", function(env) { length(env$namen) }, 5)
     testEqual("namen[1]", function(env) { env$patient_names[1] }, "Alice")
     testEqual("namen[2]", function(env) { env$patient_names[2] }, "Bob")
     testEqual("namen[3]", function(env) { env$patient_names[3] }, "Karen")
@@ -8,8 +8,8 @@ context({
     testEqual("namen[5]", function(env) { env$patient_names[5] }, "Eva")
   })
 
-  testcase('Check if antibody_counts variable is created correctly', {
-    testEqual("length(antilichamen )", function(env) { length(env$antibody_counts) }, 5)
+  testcase('Controleer de lengte van de vector bij de variabele -antilichamen-', {
+    testEqual("length(antilichamen )", function(env) { length(env$antilichamen) }, 5)
     testEqual("antilichamen [1]", function(env) { env$antibody_counts[1] }, 120)
     testEqual("antilichamen [2]", function(env) { env$antibody_counts[2] }, 150)
     testEqual("antilichamen [3]", function(env) { env$antibody_counts[3] }, 135)
