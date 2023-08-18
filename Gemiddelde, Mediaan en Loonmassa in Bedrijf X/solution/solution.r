2ef997c09a64fc0bd8127b94bfd05c07bd259557
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-08-18 17:30:56 +0200

    Update solution.r

--- Gemiddelde, Mediaan en Loonmassa in Bedrijf X/solution/solution.r
+++ Gemiddelde, Mediaan en Loonmassa in Bedrijf X/solution/solution.r
@@ -1,11 +1,11 @@
 # Gegeven
-salaries <- c(2500, 3200, 4800, 3900, 4100, 5600, 4000, 2700, 5100, 3800, 5900, 4400, 6200, 2900, 5300)
+lonen <- c(2500, 3200, 4800, 3900, 4100, 5600, 4000, 2700, 5100, 3800, 5900, 4400, 6200, 2900, 5300)
 
 # Totale som van lonen
-total_wages <- sum(salaries)
+totaal_loon <- sum(lonen)
 
 # Gemiddeld loonniveau
-average_wage <- mean(salaries)
+gemiddeld_loon <- mean(lonen)
 
 # Mediaan loon
-median_wage <- median(salaries)
+mediaan_loon <- median(lonen)
