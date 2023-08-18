41f4281944c2e8a5530c4486231ad7003ac5d437
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-08-18 14:51:46 +0200

    Update 2-x.R

--- Patiëntenonderzoek en Antilichamen/evaluation/2-x.R
+++ Patiëntenonderzoek en Antilichamen/evaluation/2-x.R
@@ -1,11 +1,11 @@
 context({
   testcase('Controleer de lengte van de vector bij variabele -namen-', {
     testEqual("length(patient_namen)", function(env) { length(env$namen) }, 5)
-    testEqual("namen[1]", function(env) { env$patient_names[1] }, "Alice")
-    testEqual("namen[2]", function(env) { env$patient_names[2] }, "Bob")
-    testEqual("namen[3]", function(env) { env$patient_names[3] }, "Karen")
-    testEqual("namen[4]", function(env) { env$patient_names[4] }, "David")
-    testEqual("namen[5]", function(env) { env$patient_names[5] }, "Eva")
+    testEqual("namen [1]", function(env) { env$patient_names[1] }, "Alice")
+    testEqual("namen [2]", function(env) { env$patient_names[2] }, "Bob")
+    testEqual("namen [3]", function(env) { env$patient_names[3] }, "Karen")
+    testEqual("namen [4]", function(env) { env$patient_names[4] }, "David")
+    testEqual("namen [5]", function(env) { env$patient_names[5] }, "Eva")
   })
 
   testcase('Controleer de lengte van de vector bij de variabele -antilichamen-', {
