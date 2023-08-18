d7077389e1621675ff92752233db0936c6970920
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-08-18 09:33:58 +0200

    Update solution.r

--- Gemiddelde Lengte in de Ploeg/solution/solution.r
+++ Gemiddelde Lengte in de Ploeg/solution/solution.r
@@ -2,7 +2,7 @@
 lengte <- c(188, 192, 185, 190, 187)
 
 # Bereken de gemiddelde lengte
-gemiddelde_lengte <- gemiddelde(lengte)
+gemiddelde_lengte <- mean(lengte)
 
 # Druk de gemiddelde lengte af
 print(gemiddelde_lengte)
