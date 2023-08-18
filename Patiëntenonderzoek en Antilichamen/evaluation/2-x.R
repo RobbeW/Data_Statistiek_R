f4a74e7f1ff7bfbbae2cd74693e47ef16c5916ab
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-08-18 14:35:21 +0200

    Update 2-x.R

--- Patiëntenonderzoek en Antilichamen/evaluation/2-x.R
+++ Patiëntenonderzoek en Antilichamen/evaluation/2-x.R
@@ -1,11 +1,11 @@
 context({
   testcase('Check if patient_names variable is created correctly', {
-    testEqual("length(patient_names)", function(env) { length(env$patient_names) }, 5)
-    testEqual("patient_names[1]", function(env) { env$patient_names[1] }, "Alice")
-    testEqual("patient_names[2]", function(env) { env$patient_names[2] }, "Bob")
-    testEqual("patient_names[3]", function(env) { env$patient_names[3] }, "Karen")
-    testEqual("patient_names[4]", function(env) { env$patient_names[4] }, "David")
-    testEqual("patient_names[5]", function(env) { env$patient_names[5] }, "Eva")
+    testEqual("length(patient_namen)", function(env) { length(env$patient_names) }, 5)
+    testEqual("patient_namen[1]", function(env) { env$patient_names[1] }, "Alice")
+    testEqual("patient_namen[2]", function(env) { env$patient_names[2] }, "Bob")
+    testEqual("patient_namen[3]", function(env) { env$patient_names[3] }, "Karen")
+    testEqual("patient_namen[4]", function(env) { env$patient_names[4] }, "David")
+    testEqual("patient_namen[5]", function(env) { env$patient_names[5] }, "Eva")
   })
 
   testcase('Check if antibody_counts variable is created correctly', {
