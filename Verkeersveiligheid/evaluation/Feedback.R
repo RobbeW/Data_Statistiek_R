9fbbd6be44c80abb49025be59bf5147eb7b40e03
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-08-28 00:05:05 +0200

    Better feedback

--- Verkeersveiligheid/evaluation/Feedback.R
+++ Verkeersveiligheid/evaluation/Feedback.R
@@ -1,18 +1,21 @@
 context({
-  testcase("doden en km_gereden zijn correct", {
+  testcase("Variabelen zijn correct:", {
     testEqual("aantal_doden", function(env) {
       env$aantal_doden
     }, c(716, 510))
     testEqual("aantal_miljoen_gereden_km", function(env) {
       env$aantal_miljoen_gereden_km
     }, c(84.2, 92.9) * 10^3)
+    testEqual("risico", function(env) {
+      env$risico
+    }, c(0.008503563, 0.005489774))
   })
   testcase("Risico is correct berekend:", {
     testEqual("risico[1]", function(env) {
-      env$risico[1]
+      env$aantal_doden[1] / env$aantal_miljoen_gereden_km[1]
     }, 0.008503563)
     testEqual("risico[2]", function(env) {
-      env$risico[2]
+      env$aantal_doden[2] / env$aantal_miljoen_gereden_km[2]
     }, 0.005489774)
   })
 })
