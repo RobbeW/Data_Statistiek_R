311193950fb5b59ca4f8e1c64229dbf5e92d87b5
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-08-28 23:57:00 +0200

    Final fixes

--- Deel 1 Beschrijvend/01 Inleiding/Bluegill fish/evaluation/Feedback.R
+++ Deel 1 Beschrijvend/01 Inleiding/Bluegill fish/evaluation/Feedback.R
@@ -1,10 +1,10 @@
 context({
-  testcase("Variabel werden correct gedeclareerd:", {
+  testcase("Variabele werden correct gedeclareerd:", {
     testEqual("leeftijden", function(env) {
       env$leeftijden
     }, c(2, 4, 6, 8, 10))
   })
-  testcase("Variabel werden correct berekend:", {
+  testcase("Variabele werden correct berekend:", {
     testEqual("schatting_lengtes", function(env) {
       env$schatting_lengtes
     }, c(104.984, 162.936, 187.456, 178.544, 136.2))
