46f0bd023237db2eada97fb510e5488a6637791a
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-08-30 01:00:20 +0200

    Typo fix

--- Deel 1 Beschrijvend/02 Centrummaten/Remtest/solution/solution.nl.R
+++ Deel 1 Beschrijvend/02 Centrummaten/Remtest/solution/solution.nl.R
@@ -4,8 +4,8 @@ data <- read.table("https://tinyurl.com/4p3znsv9", header = TRUE)
 # De mediaan berekenen
 n <- length(data$remweg)
 if (n %% 2 != 0) { # oneven aantal waarnemingen
-  mediane_remweg <- data$remweg[(n + 1) / 2]
+  mediaan_remweg <- data$remweg[(n + 1) / 2]
 } else { # even aantal waarnemingen
   middelste_waarden <- c(data$remweg[n / 2], data$remweg[n / 2 + 1])
-  mediane_remweg <- mean(middelste_waarden)
+  mediaan_remweg <- mean(middelste_waarden)
 }
