44c34b06f4ac4dbd13c95ae718db274f9a1770b1
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-08-29 18:39:11 +0200

    Feedback fix

--- Deel 1 Beschrijvend/01 Inleiding/Basketballplayers/evaluation/Feedback.R
+++ Deel 1 Beschrijvend/01 Inleiding/Basketballplayers/evaluation/Feedback.R
@@ -1,7 +1,7 @@
 context({
   testcase("De variabelen zijn correct:", {
     testEqual("lengtes", function(env) {
-      env$lengte
+      env$lengtes
     }, c(198, 195, 193, 202, 185, 186, 172, 185))
     testEqual("lengte_speler4", function(env) {
       env$lengte_speler4
