a06746c9efa082dda8f91c4f495ee4fe69442faa
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-09-03 10:13:04 +0200

    Extra tests

--- Deel 1 Beschrijvend/02 Centrummaten/Olympisch gemiddelde/evaluation/Feedback.R
+++ Deel 1 Beschrijvend/02 Centrummaten/Olympisch gemiddelde/evaluation/Feedback.R
@@ -1,13 +1,21 @@
 context({
-  scores <- c(98, 97, 98, 99, 100, 98)
-  resultaten <- c(14, 25, 14, 18, 16, 16, 20)
+  test1 <- c(98, 97, 98, 99, 100, 98)
+  test2 <- c(14, 25, 14, 18, 16, 16, 20)
+  test3 <- c(102, 15, 30, 25)
+  test4 <- c(101, 101, 101, 101, 101, 101, 101, 101)
 
   testcase("De functie werkt:", {
-    testEqual("Voor invoer `c(98, 97, 98, 99, 100, 98)`", function(env) {
-      env$olympic_mean(scores)
+    testEqual("met parameter c(98, 97, 98, 99, 100, 98)", function(env) {
+      env$olympic_mean(test1)
     }, 98.25)
-    testEqual("Voor invoer `c(14, 25, 14, 18, 16, 16, 20)`", function(env) {
-      env$olympic_mean(resultaten)
+    testEqual("met parameter c(14, 25, 14, 18, 16, 16, 20)", function(env) {
+      env$olympic_mean(test2)
     }, 16.8)
+    testEqual("met parameter c(102, 15, 30, 25)", function(env) {
+      env$olympic_mean(test3)
+    }, 27.5)
+    testEqual("met parameter c(101, 101, 101, 101, 101, 101, 101, 101)", function(env) {
+      env$olympic_mean(test4)
+    }, 101)
   })
 })
