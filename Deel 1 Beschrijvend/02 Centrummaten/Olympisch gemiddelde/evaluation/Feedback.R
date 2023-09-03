4178a9d44e63ca6e16a583424af652b68fd67109
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-09-03 10:10:34 +0200

    Feedback fix

--- Deel 1 Beschrijvend/02 Centrummaten/Olympisch gemiddelde/evaluation/Feedback.R
+++ Deel 1 Beschrijvend/02 Centrummaten/Olympisch gemiddelde/evaluation/Feedback.R
@@ -3,10 +3,10 @@ context({
   resultaten <- c(14, 25, 14, 18, 16, 16, 20)
 
   testcase("De functie werkt:", {
-    testEqual("c(98, 97, 98, 99, 100, 98)", function(env) {
+    testEqual("Voor invoer `c(98, 97, 98, 99, 100, 98)`", function(env) {
       env$olympic_mean(scores)
     }, 98.25)
-    testEqual("c(14, 25, 14, 18, 16, 16, 20)", function(env) {
+    testEqual("Voor invoer `c(14, 25, 14, 18, 16, 16, 20)`", function(env) {
       env$olympic_mean(resultaten)
     }, 16.8)
   })
