a9531321ffc2fd4f2398635208437f99238fefc4
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-09-15 22:23:02 +0200

    Expanded tests

--- Deel 1 Beschrijvend/01 Inleiding/Patiëntenonderzoek en antilichamen/evaluation/Feedback.R
+++ Deel 1 Beschrijvend/01 Inleiding/Patiëntenonderzoek en antilichamen/evaluation/Feedback.R
@@ -1,4 +1,25 @@
 context({
+  testcaseAssert("De variabele namen bestaat.", function(env) {
+    isTRUE(exists("namen", env))
+  })
+  testcaseAssert("De variabele antistoffen_basis bestaat.", function(env) {
+    isTRUE(exists("antistoffen_basis", env))
+  })
+  testcaseAssert("De variabele antistoffen_booster bestaat.", function(env) {
+    isTRUE(exists("antistoffen_booster", env))
+  })
+  testcaseAssert("De variabele toenames bestaat.", function(env) {
+    isTRUE(exists("toenames", env))
+  })
+})
+context({
+  namen <- c("Alice", "Bob", "Karen", "David", "Eva")
+  antistoffen_basis <- c(3705, 3601, 4100, 2574, 3910)
+  antistoffen_booster <- c(22293, 14571, 25415, 18971, 38600)
+  coviddata <- data.frame("namen" = namen,
+                          "basis" = antistoffen_basis,
+                          "booster" = antistoffen_booster)
+
   testcase("De variabelen zijn correct:", {
     testEqual("namen", function(env) {
       env$namen
@@ -14,11 +35,8 @@ context({
     }, c(18588, 10970, 21315, 16397, 34690))
   })
   testcase("De data frame werd correct aangemaakt:", {
-    testEqual("kolommen", function(env) {
-      colnames(env$coviddata)
-    }, c("namen", "basis", "booster"))
-    testEqual("dimensies", function(env) {
-      dim(env$coviddata)
-    }, c(5L, 3L))
+    testDF("coviddata", function(env) {
+      env$coviddata
+    }, coviddata)
   })
 })
