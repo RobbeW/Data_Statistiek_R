eae8444d21bb33f516d841cb4b9a2e9a283becb7
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-09-15 23:26:14 +0200

    Typo fix

--- Deel 1 Beschrijvend/99 Evaluaties/Bloeddruk/solution/solution.nl.R
+++ Deel 1 Beschrijvend/99 Evaluaties/Bloeddruk/solution/solution.nl.R
@@ -2,5 +2,5 @@
 dia_druk <- 101.14
 gem_druk <- 122.54
 
-# Bepaal hieronder de schatting voor het hartslag in rust
+# Berekening van de systolische druk
 sys_druk <- round((gem_druk - dia_druk) * 3 + dia_druk, 2)
