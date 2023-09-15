8565a1d7f4c6937434b68d3d7b9d364cb062e6ba
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-09-15 21:51:06 +0200

    Variable checks added.

--- Deel 1 Beschrijvend/01 Inleiding/Capture recapture/evaluation/Feedback.R
+++ Deel 1 Beschrijvend/01 Inleiding/Capture recapture/evaluation/Feedback.R
@@ -1,4 +1,15 @@
 context({
+  testcaseAssert("De variabele n bestaat.", function(env) {
+    isTRUE(exists("n", env))
+  })
+  testcaseAssert("De variabele s bestaat.", function(env) {
+    isTRUE(exists("s", env))
+  })
+  testcaseAssert("De variabele schatting bestaat.", function(env) {
+    isTRUE(exists("schatting", env))
+  })
+})
+context({
   testcase("Variabelen werden correct gedeclareerd:", {
     testEqual("n", function(env) {
       env$n
