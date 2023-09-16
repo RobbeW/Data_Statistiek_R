0cb89116f9d9156e726a6c7c12467d8baab48c06
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-09-16 19:41:08 +0200

    Solution and feedback fix

--- Deel 1 Beschrijvend/99 Evaluaties/Smartphone straling/evaluation/Feedback.R
+++ Deel 1 Beschrijvend/99 Evaluaties/Smartphone straling/evaluation/Feedback.R
@@ -10,17 +10,17 @@ context({
   testcase("Variabelen werden correct bepaald:", {
     testEqual("niet_conform", function(env) {
       env$niet_conform
-    }, c(TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
-FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
+    }, c(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
+TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
 FALSE))
     testEqual("modellen_conform", function(env) {
       env$modellen_conform
-    }, c("Asus Zenfone 8", "OnePlus 9 Pro 5G", "Xiaomi Poco F3", "Xiaomi Mi 11i", 
-"Xiaomi Redmi Note 10S", "Sony Xperia 5III", "Samsung Galaxy Xcover 5 EE", 
-"Samsung Galaxy A03S", "Xiaomi Poco X3", "Samsung Galaxy S20 FE", 
-"Apple Galaxy Z Flip", "Apple iPhone SE 2020", "Samsung iPhone 12 Pro Max", 
-"Nokia 4.2", "Xiaomi Poco F2 Pro", "Motorola Moto G10", "Xiaomi Redmi 9A"
-))
+    }, c("OnePlus 7 Pro", "Asus Zenfone 8", "OnePlus 9 Pro 5G", "Xiaomi Poco F3", 
+"Xiaomi Mi 11i", "Xiaomi Redmi Note 10S", "Sony Xperia 5III", 
+"Samsung Galaxy Xcover 5 EE", "Samsung Galaxy A03S", "Samsung Galaxy S20 FE", 
+"Apple iPhone 12 5G", "Apple Galaxy Z Flip", "Apple iPhone SE 2020", 
+"Samsung iPhone 12 Pro Max", "Nokia 4.2", "Xiaomi Poco F2 Pro", 
+"Motorola Moto G10", "Xiaomi Redmi 9A"))
   })
 
 })
