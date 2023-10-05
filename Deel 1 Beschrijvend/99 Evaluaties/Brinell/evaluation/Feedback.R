32b3a76434dac22d148d9b703ae119a72c951598
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-10-05 20:57:04 +0200

    Splitted evaluation

--- Deel 1 Beschrijvend/99 Evaluaties/Brinell/evaluation/Feedback.R
+++ Deel 1 Beschrijvend/99 Evaluaties/Brinell/evaluation/Feedback.R
@@ -14,18 +14,30 @@ context({
     testEqual("kracht_F", function(env) {
       env$kracht_F
     }, 29500)
+  })
+})
+
+context({
+  testcase("Variabele werden correct aangemaakt:", {
     testEqual("diameter_d", function(env) {
       env$diameter_d
     }, c(5.6082, 4.0982, 6.7124, 8.8425, 7.2415, 3.0014, 1.5647))
   })
+})
+
+context({
   testcase("Variabelen werden correct berekend:", {
     testEqual("brinell", function(env) {
       env$brinell
     }, c(111.3312, 218.093, 74.0296, 35.9407, 61.722, 415.4854, 1555.2034))
     testFunctionUsedInVar("round", "brinell")
+  })
+})
+
+context({
+  testcase("Variabelen werden correct berekend:", {
     testEqual("harde_materialen", function(env) {
       env$harde_materialen
     }, c("Gehardend staal", "Gietijzer", "Glas"))
   })
-
 })
