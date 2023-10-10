e0697c72193ddec75636af2a4aad5470a6fe9a82
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-10-10 09:03:11 +0200

    Fixed evaluation

--- Deel 1 Beschrijvend/02 Centrummaten/Gemiddelde lengte in de ploeg - finaal/evaluation/Feedback.R
+++ Deel 1 Beschrijvend/02 Centrummaten/Gemiddelde lengte in de ploeg - finaal/evaluation/Feedback.R
@@ -1,13 +1,21 @@
 context({
-  testcase("Gemiddeldes werden correct berekend:", {
+  testcase("Gemiddelde lengte van de mannen werd correct berekend:", {
     testEqual("gemiddelde_mannen", function(env) {
       env$gemiddelde_mannen
     }, 197)
+  })
+  testcase("De volgende functie werden gebruikt:", {
+    testFunctionUsedInVar("mean", "gemiddelde_mannen")
+  })
+})
+
+context({
+  testcase("Gemiddelde lengte van de vrouwen werd correct berekend:", {
     testEqual("gemiddelde_vrouwen", function(env) {
       env$gemiddelde_vrouwen
     }, 182)
   })
   testcase("De volgende functie werden gebruikt:", {
-    testFunctionUsed("mean")
+    testFunctionUsedInVar("mean", "gemiddelde_vrouwen")
   })
 })
--- Deel 1 Beschrijvend/02 Centrummaten/Gemiddelde lengte in de ploeg - uitbreiding/evaluation/Feedback.R
+++ Deel 1 Beschrijvend/02 Centrummaten/Gemiddelde lengte in de ploeg - uitbreiding/evaluation/Feedback.R
@@ -1,8 +1,20 @@
 context({
-  testcase("Gemiddeldes werden correct berekend:", {
+  testcase("Gemiddelde lengte van de mannen werd correct berekend:", {
     testEqual("gemiddelde_mannen", function(env) {
       env$gemiddelde_mannen
     }, 197)
+  })
+  testcaseAssert("De variabele som bestaat niet meer:", function(env) {
+    isTRUE(!exists("som", env))
+  })
+  testcase("De volgende functies werden gebruikt:", {
+    testFunctionUsedInVar("sum", "gemiddelde_mannen")
+    testFunctionUsedInVar("length", "gemiddelde_mannen")
+  })
+})
+
+context({
+  testcase("Gemiddelde lengte van de vrouwen werd correct berekend:", {
     testEqual("gemiddelde_vrouwen", function(env) {
       env$gemiddelde_vrouwen
     }, 182)
@@ -11,8 +23,7 @@ context({
     isTRUE(!exists("som", env))
   })
   testcase("De volgende functies werden gebruikt:", {
-    testFunctionUsed("sum")
-    testFunctionUsed("length")
+    testFunctionUsedInVar("sum", "gemiddelde_vrouwen")
+    testFunctionUsedInVar("length", "gemiddelde_vrouwen")
   })
-
 })
