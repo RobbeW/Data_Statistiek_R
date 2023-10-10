5763bd2a5fe84e76af5b5ee54e237a2fb4caf40b
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-10-10 08:59:04 +0200

    Fix evaluation

--- Deel 1 Beschrijvend/02 Centrummaten/Gemiddelde lengte in de ploeg - basis/evaluation/Feedback.R
+++ Deel 1 Beschrijvend/02 Centrummaten/Gemiddelde lengte in de ploeg - basis/evaluation/Feedback.R
@@ -1,8 +1,17 @@
 context({
-  testcase("Gemiddeldes werden correct berekend:", {
+  testcase("Gemiddelde lengte van de mannen werd correct berekend:", {
     testEqual("gemiddelde_mannen", function(env) {
       env$gemiddelde_mannen
     }, 197)
+  })
+  testcase("De volgende functies werden gebruikt:", {
+    testFunctionUsed("for")
+    testFunctionUsed("length")
+  })
+})
+
+context({
+  testcase("Gemiddeldes lengte van de vrouwen werd correct berekend:", {
     testEqual("gemiddelde_vrouwen", function(env) {
       env$gemiddelde_vrouwen
     }, 182)
