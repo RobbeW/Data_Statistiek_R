ce7ae6cf0a31ad3ca0f7b3278ed20458740ff565
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-10-10 09:07:07 +0200

    Splitted feedback

--- Deel 1 Beschrijvend/02 Centrummaten/Langdurig zieken/evaluation/Feedback.R
+++ Deel 1 Beschrijvend/02 Centrummaten/Langdurig zieken/evaluation/Feedback.R
@@ -8,20 +8,38 @@ context({
     }, c(250000, 266667, 283334, 300001, 316668, 333335, 350002, 366669,
          383336, 400003, 416670, 433337, 450004, 466671, 483338, 500005))
   })
+})
+
+
+context({
   testcase("De berekeningen werden correct uitgevoerd:", {
     testEqual("totaal_verloren_arbeidsjaren", function(env) {
       env$totaal_verloren_arbeidsjaren
     }, 6000040)
+  })
+  testcase("De volgende functies werden gebruikt:", {
+    testFunctionUsedInVar("sum", "totaal_verloren_arbeidsjaren")
+  })
+})
+
+context({
+  testcase("De berekeningen werden correct uitgevoerd:", {
     testEqual("gemiddeld_aantal_zieken", function(env) {
       env$gemiddeld_aantal_zieken
     }, 375002.5)
+  })
+  testcase("De volgende functies werden gebruikt:", {
+    testFunctionUsedInVar("mean", "gemiddeld_aantal_zieken")
+  })
+})
+
+context({
+  testcase("De berekeningen werden correct uitgevoerd:", {
     testEqual("aantal_jaren_met_veel_zieken", function(env) {
       env$aantal_jaren_met_veel_zieken
     }, 8)
   })
   testcase("De volgende functies werden gebruikt:", {
-    testFunctionUsedInVar("sum", "totaal_verloren_arbeidsjaren")
-    testFunctionUsedInVar("mean", "gemiddeld_aantal_zieken")
     testFunctionUsedInVar("length", "aantal_jaren_met_veel_zieken")
   })
 })
