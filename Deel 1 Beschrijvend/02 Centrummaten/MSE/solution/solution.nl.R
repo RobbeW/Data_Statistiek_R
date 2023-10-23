f249ae9256b89614d82d1d8e23df99a79d85658a
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-10-24 00:08:44 +0200

    Fix solution

--- Deel 1 Beschrijvend/02 Centrummaten/MSE/solution/solution.nl.R
+++ Deel 1 Beschrijvend/02 Centrummaten/MSE/solution/solution.nl.R
@@ -1,13 +1,13 @@
 mse <- function(echte_gegevens, voorspelde_gegevens) {
   result <- mean( (echte_gegevens - voorspelde_gegevens)^2)
-  return(round(result, 2))
+  return(round(result, 3))
 }
 
 ### Enkele testen ###
 echte_gegevens <- c(1000.02, 1000.96, 1001.82, 1002.75)
 voorspelde_gegevens <- c(999.96, 1001.00, 1002.04, 1002.56)
-mse(echte_gegevens, voorspelde_gegevens)  # 0.02, dit is klein, dus de voorspelling is goed!
+mse(echte_gegevens, voorspelde_gegevens)  # 0.022, dit is klein, dus de voorspelling is goed!
 
 echte_gegevens <- c(1000.02, 1000.96, 1001.82, 1002.75)
 voorspelde_gegevens <- c(950.34, 987.15, 1011.71, 1034.18)
-mse(echte_gegevens, voorspelde_gegevens)  # 936.12, hier is de voorspelling duidelijk slechter!
+mse(echte_gegevens, voorspelde_gegevens)  # 936.119, hier is de voorspelling duidelijk slechter!
