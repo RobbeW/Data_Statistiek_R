41f77876122b82e269b2a68be7450ccb031ca694
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2024-01-06 16:04:52 +0100

    Fixing feedback

--- Deel 1 Beschrijvend/04 Grafische weergaven/Blok in Gent/evaluation/Feedback.R
+++ Deel 1 Beschrijvend/04 Grafische weergaven/Blok in Gent/evaluation/Feedback.R
@@ -3,16 +3,15 @@ data <- read.csv2("https://data.stad.gent/api/explore/v2.1/catalog/datasets/blok
                   sep = ";",
                   colClasses = c("NULL", "character", rep("NULL", 5), rep("numeric", 2), rep("NULL", 4), "character", rep("NULL", 5)) )
 colnames(data) <- c("naam", "capaciteit", "gereserveerd", "status")
-data <- data[data$status == "Reserveerbaar" && data$capaciteit != 0,1:3]
+data <- data[data$status == "Reserveerbaar",1:3]
 data <- data[order(data$capaciteit, decreasing=TRUE),]
 rownames(data) <- seq_len(nrow(data))
 
-# Plot
 reserveringsgraad <- round(data$gereserveerd/data$capaciteit * 100, 1)
 
 context({
   testcaseAssert("De variabele reserveringsgraad bestaat.", function(env) {
-    isTRUE(exists("partijen", env))
+    isTRUE(exists("reserveringsgraad", env))
   })
   testcase("De variabele werd correct bepaald:", {
     testEqual("reserveringsgraad", function(env) {
