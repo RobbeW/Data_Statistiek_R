be90fb9df12016759558565c4a77b0dfdd28266d
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2024-02-20 17:01:52 +0100

    Fixed typo in plot

--- Deel 1 Statistiek/04 Grafische weergaven/Z - Evaluatie/Lego/description/media/plots.R
+++ Deel 1 Statistiek/04 Grafische weergaven/Z - Evaluatie/Lego/description/media/plots.R
@@ -1,12 +1,12 @@
 # Importeert de data
 data <- read.csv2("https://www.openintro.org/data/csv/lego_population.csv",
                   sep = ",")
-data <- data[,c(2,3,4,5,7,8)]
+data <- data[, c(2, 3, 4, 5, 7, 8)]
 data$price <- as.numeric(data$price)
 data <- data[complete.cases(data),]
 
 # Antwoord op de vragen
-thema_city = data$theme == "City"
+thema_city <- data$theme == "City"
 
 res <- 150
 # Plot
@@ -20,7 +20,7 @@ par(fg = "black")
 plot(data$price[thema_city]~data$pieces[thema_city],
      main = "Prijs versus aantal blokken",
      xlab = "aantal blokken",
-     ylab = "winkeprijs (in $)",
+     ylab = "winkelprijs (in $)",
      pch = 4)
 abline(lm(data$price[thema_city]~data$pieces[thema_city]), 
        col="red")
@@ -37,7 +37,7 @@ par(fg = "white")
 plot(data$price[thema_city]~data$pieces[thema_city],
      main = "Prijs versus aantal blokken",
      xlab = "aantal blokken",
-     ylab = "winkeprijs (in $)",
+     ylab = "winkelprijs (in $)",
      pch = 4,
         border = "white",
         col.main = "white",
