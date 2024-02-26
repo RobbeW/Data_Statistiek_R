63a6b50a61e0a1899d8199c83fa77177e60e7167
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2024-02-26 22:17:12 +0100

    Initial Reuleaux exercise

---
+++ Deel 2 Python/01 Herhaling/06 - Reuleaux/preparation/generator.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import importlib
+import random
+import ruamel.yaml
+import subprocess
+
+yaml = ruamel.yaml.YAML()
+
+# set fixed seed for generating test cases
+random.seed(12345678)
+
+# locate evaldir
+evaldir = os.path.join('..', 'evaluation')
+if not os.path.exists(evaldir):
+    os.makedirs(evaldir)
+
+# locate solutiondir
+solutiondir = os.path.join('..', 'solution')
+if not os.path.exists(solutiondir):
+    os.makedirs(solutiondir)
+
+def write_yaml( data:list ):
+    """ A function to write YAML file"""
+    with open(os.path.join('..', 'evaluation', 'tests.yaml'), 'w', encoding='utf-8') as f:
+        yaml.dump(data, f)
+# generate test data
+ntests= 20
+cases = [(5,) ,(1.12,)]
+while len(cases) < ntests:
+    z = round( random.uniform( 1, 10), 3)
+    cases.append( (z, ) )
+    
+# generate unit tests for functions
+yamldata = []
+yamldata.append( {'tab': 'Feedback', 'testcases': []})
+# input, expression, statement or stdin?
+
+input = 'stdin'
+# output, stdout or return?
+output = 'stdout'
+
+for test in cases:
+    # generate test expression
+    # add input to input file
+    stdin = '\n'.join(f'{line}' for line in test)
+
+    # generate output to output file
+    script = os.path.join(solutiondir, 'solution.nl.py')
+    process= subprocess.run(
+        ['python3', script],
+        input=stdin,
+        encoding='utf-8',
+        capture_output=True
+    )
+    
+    result_lines = process.stdout.split("\n")
+    result_lines = [x for x in result_lines[len(test):]]
+    for line in result_lines:
+        if not(line.startswith( 'Geef' )):
+            print(line)
+    
+    # setup for return expressions
+    testcase = { input: stdin, output: result_lines.pop(0) }
+    yamldata[0]['testcases'].append( testcase)
+
+write_yaml(yamldata)
