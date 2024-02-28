293fdb6c054d3d0599f4f2bd6ce9d4d07110933a
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2024-02-28 20:40:24 +0100

    Fix feedback

--- Deel 1 Statistiek/04 Grafische weergaven/Z - Evaluatie/Bevolking Gent per wijk/evaluation/Feedback.R
+++ Deel 1 Statistiek/04 Grafische weergaven/Z - Evaluatie/Bevolking Gent per wijk/evaluation/Feedback.R
@@ -13,7 +13,7 @@ laatste <- data$jaar == 2023
 totaal <- sum(data$aantal[laatste])
 percentages <- round(data$aantal[laatste] / totaal * 100, 0)
 
-labels <- paste0(data$wijk[laatste], " - ", percentages,"%")
+labels <- paste0(data$wijk[laatste], " (", percentages,"%)")
 
 context({
   testcaseAssert("De variabele laatste bestaat.", function(env) {
