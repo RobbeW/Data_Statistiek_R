c1b19b9e4c2212f296fb9f042ed4ff7ee4d2d8ca
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2024-03-04 23:36:43 +0100

    Fixing solution and checks

--- Deel 2 Python/01 Herhaling/07 - Kinepolis/solution/solution.nl.py
+++ Deel 2 Python/01 Herhaling/07 - Kinepolis/solution/solution.nl.py
@@ -3,7 +3,7 @@ leeftijd = int( input( 'Geef je leeftijd in: ' ) )
 speeltijd = int( input( 'Geef de speeltijd in: ' ) )
 
 # Berekening
-prijs = 14.80
+prijs = 13.80
 if leeftijd <= 18 or leeftijd >= 60:
     prijs = prijs - 1
     
