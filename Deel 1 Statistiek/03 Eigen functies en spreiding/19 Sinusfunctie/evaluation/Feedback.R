8fbc41d33e4d47af34b7f21c3a14b9a607bb72d5
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-07-31 00:21:06 +0200

    Code formatting

--- Deel 1 Statistiek/03 Eigen functies en spreiding/19 Sinusfunctie/evaluation/Feedback.R
+++ Deel 1 Statistiek/03 Eigen functies en spreiding/19 Sinusfunctie/evaluation/Feedback.R
@@ -3,18 +3,18 @@ set.seed(1234)
 # Een functie die de benadering simuleert
 sin_benadering <- function(aantal) {
   rij <- 1:aantal
-  resultaat <- pi / 3 *prod( 1-1/(9*rij^2)) 
+  resultaat <- pi / 3 * prod(1 - 1 / (9 * rij^2))
   return(round(resultaat, 6))
 }
 
-rijtje <- c(2,3,4,5)
+rijtje <- c(2, 3, 4, 5)
 rijtje <- c(rijtje, 10^(1:4))
 
 context({
   testcase("De functie sin_benadering(aantal) werkt met de volgende parameters:", {
     for (aantal in rijtje) {
       testEqual(paste("aantal =",
-                      format(aantal, scientific = FALSE)), 
+                      format(aantal, scientific = FALSE)),
                 function(env) {
                   env$sin_benadering(aantal)
                 }, sin_benadering(aantal))
