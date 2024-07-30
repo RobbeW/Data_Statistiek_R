7ec6f82c8a80978d95835a5fcc94de381fb8d43e
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-07-30 21:32:18 +0200

    Better feedback

--- Deel 1 Statistiek/03 Eigen functies en spreiding/13 Faculteit/evaluation/Feedback.R
+++ Deel 1 Statistiek/03 Eigen functies en spreiding/13 Faculteit/evaluation/Feedback.R
@@ -12,9 +12,9 @@ nsim <- 20
 cases <- 0:10
 
 context({
-  testcase("De functie faculteit() werkt met de volgende parameters:", {
+  testcase("De functie faculteit(getal) werkt met de volgende parameters:", {
     for (case in cases) {
-      testEqual(case, function(env) {
+      testEqual(paste("getal = ",case), function(env) {
         format(env$faculteit(case), scientific = FALSE)
       }, format(faculteit(case), scientific = FALSE))
     }
