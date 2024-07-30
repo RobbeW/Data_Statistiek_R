66759da644f25dafa4e76a9b5752e2306aa9ce1c
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-07-30 21:27:44 +0200

    Fixing Leibniz

--- Deel 1 Statistiek/03 Eigen functies en spreiding/12 Leibniz formule/evaluation/Feedback.R
+++ Deel 1 Statistiek/03 Eigen functies en spreiding/12 Leibniz formule/evaluation/Feedback.R
@@ -1,17 +1,17 @@
 context({
   leibniz <- function(n) {
     rij <- 1:n
-    alter <- (-1)^(rij+1)
-    oneven <- rij*2 - 1
+    alter <- (-1)^(rij + 1)
+    oneven <- rij * 2 - 1
     res <- alter * oneven
-    return(round(sum(1/res), 6))
+    return(round(sum(1 / res), 6))
   }
 
-  testcase("De functie leibniz werkt:", {
+  testcase("De functie leibniz() werkt met de volgende parameters:", {
     rijtje <- 1:6
     for (aantal in rijtje) {
-      testEqual(paste0("met parameter ", 
-                       format(10^aantal, scientific = FALSE)), function(env) {
+      testEqual(format(10^aantal, scientific = FALSE),
+                function(env) {
                   env$leibniz(10^aantal)
                 }, leibniz(10^aantal))
     }
--- Deel 1 Statistiek/03 Eigen functies en spreiding/12 Leibniz formule/solution/solution.nl.R
+++ Deel 1 Statistiek/03 Eigen functies en spreiding/12 Leibniz formule/solution/solution.nl.R
@@ -1,10 +1,10 @@
 # Een functie die het probleem van Leibniz simuleert
 leibniz <- function(n) {
   rij <- 1:n
-  alter <- (-1)^(rij+1)
-  oneven <- rij*2 - 1
+  alter <- (-1)^(rij + 1)
+  oneven <- rij * 2 - 1
   res <- alter * oneven
-  return(round(sum(1/res), 6))
+  return(round(sum(1 / res), 6))
 }
 
 # Mogelijke controle
