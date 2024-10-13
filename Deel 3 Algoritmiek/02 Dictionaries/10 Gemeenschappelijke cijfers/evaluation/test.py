05d1a2c0d611bc49b622d99001f18b3ae6a71aa3
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-10-13 22:02:01 +0200

    trying custom check function

---
+++ Deel 3 Algoritmiek/02 Dictionaries/10 Gemeenschappelijke cijfers/evaluation/test.py
@@ -0,0 +1,6 @@
+from evaluation_utils import EvaluationResult, ConvertedOracleContext
+
+def check_function(context: ConvertedOracleContext, *) -> EvaluationResult:
+    value = (context.expected == context.actual)
+    obj = EvaluationResult(result = value)
+    return obj
