322c0c170894f12c6b71b10d49eb643ab46584c4
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-10-31 17:52:24 +0100

    fixing tests

--- Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/01 Voornamen/evaluation/test.py
+++ Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/01 Voornamen/evaluation/test.py
@@ -4,7 +4,7 @@ def check_function(context: ConvertedOracleContext) -> EvaluationResult:
     # expected as defined in the value field of the test suit
     # actual as given by running the file
     value = set(context.expected.keys()) == set(context.actual.keys())
-    for key, voornamen in context.expected:
+    for key, voornamen in context.expected.items():
         value &= set(voornamen) == set(context.actual[key])
         value &= type(voornamen) == type(context.actual[key])
 
