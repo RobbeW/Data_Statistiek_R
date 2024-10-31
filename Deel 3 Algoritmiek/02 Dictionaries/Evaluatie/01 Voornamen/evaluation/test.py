99347e9c284e4813958e2cac5204bec2e78d962f
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-10-31 17:51:48 +0100

    Trying custom check

---
+++ Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/01 Voornamen/evaluation/test.py
@@ -0,0 +1,12 @@
+from evaluation_utils import EvaluationResult, ConvertedOracleContext
+
+def check_function(context: ConvertedOracleContext) -> EvaluationResult:
+    # expected as defined in the value field of the test suit
+    # actual as given by running the file
+    value = set(context.expected.keys()) == set(context.actual.keys())
+    for key, voornamen in context.expected:
+        value &= set(voornamen) == set(context.actual[key])
+        value &= type(voornamen) == type(context.actual[key])
+
+    obj = EvaluationResult(result = value)
+    return obj
