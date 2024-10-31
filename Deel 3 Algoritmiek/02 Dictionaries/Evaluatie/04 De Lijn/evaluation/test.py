ad99e0564ae049d6fb1a900c04861105c328bd75
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-10-31 17:39:09 +0100

    trying to revert

--- Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/04 De Lijn/evaluation/test.py
+++ Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/04 De Lijn/evaluation/test.py
@@ -3,9 +3,15 @@ from evaluation_utils import EvaluationResult, ConvertedOracleContext
 def check_function(context: ConvertedOracleContext) -> EvaluationResult:
     # expected as defined in the value field of the test suit
     # actual as given by running the file
+<<<<<<< HEAD
     value = sorted(context.expected.keys()) == sorted(context.actual.keys())
     for key, value in context.expected.items():
         value &= sorted(value) == sorted(context.actual[key])
+=======
+    value = set(context.expected.keys()) == set(context.actual.keys())
+    for key, value in context.expected.items():
+        value &= set(value) == set(context.actual[key])
+>>>>>>> 4946f6e (fixing test)
     
     obj = EvaluationResult(result = value)
     return obj
