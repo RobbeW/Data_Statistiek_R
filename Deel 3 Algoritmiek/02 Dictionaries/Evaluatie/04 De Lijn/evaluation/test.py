f1fd805137b5f97ef85349ea220e01ec08e73b81
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-10-31 17:40:31 +0100

    attempt to fix

--- Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/04 De Lijn/evaluation/test.py
+++ Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/04 De Lijn/evaluation/test.py
@@ -3,9 +3,9 @@ from evaluation_utils import EvaluationResult, ConvertedOracleContext
 def check_function(context: ConvertedOracleContext) -> EvaluationResult:
     # expected as defined in the value field of the test suit
     # actual as given by running the file
-    value = set(context.expected.keys()) == set(context.actual.keys())
+    value = sorted(context.expected.keys()) == sorted(context.actual.keys())
     for key, value in context.expected.items():
-        value &= set(value) == set(context.actual[key])
+        value &= sorted(value) == sorted(context.actual[key])
     
     obj = EvaluationResult(result = value)
     return obj
