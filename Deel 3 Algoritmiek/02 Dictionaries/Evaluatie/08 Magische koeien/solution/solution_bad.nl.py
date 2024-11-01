bbf9caeaa489f1af35b497d1c26ec75f2b873bf2
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-11-01 13:10:26 +0100

    fixing solution bad

--- Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/08 Magische koeien/solution/solution_bad.nl.py
+++ Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/08 Magische koeien/solution/solution_bad.nl.py
@@ -1,7 +1,7 @@
 import math
 
 def koeien(C, weilanden, dag):
-    # looping solution
+    # looping solution, grow the list each day, but this can grow nearly exponentially!
     for _ in range(dag):
         weilanden_nieuw = []
         for aantal in weilanden:
@@ -11,5 +11,6 @@ def koeien(C, weilanden, dag):
             else:
                 weilanden_nieuw.append(math.floor(nieuw/2))
                 weilanden_nieuw.append(math.ceil(nieuw/2))
+        weilanden = weilanden_nieuw
     
     return len(weilanden)
