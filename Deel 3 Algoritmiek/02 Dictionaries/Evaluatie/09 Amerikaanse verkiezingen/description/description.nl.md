7bc262bf5f7f8e8f3c1c76b65f4af92f69f44aa9
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-11-06 23:28:33 +0100

    Initial american election exercise

---
+++ Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/09 Amerikaanse verkiezingen/description/description.nl.md
@@ -0,0 +1,39 @@
+De Amerikaanse president wordt verkozen via kiesmannen. Afhankelijk van de grootte van een Amerikaanse staat worden er meer of minder kiesmannen gekozen. In de volgende dictionary vind je een overzicht van deze aantallen:
+
+```python
+staten = {
+    "Alabama": 9, "Alaska": 3, "Arizona": 11, "Arkansas": 6, "California": 55, "Colorado": 9, "Connecticut": 7, "Delaware": 3, "District of Columbia": 3,
+    "Florida": 29, "Georgia": 16, "Hawaii": 4, "Idaho": 4, "Illinois": 20, "Indiana": 11, "Iowa": 6, "Kansas": 6, "Kentucky": 8, "Louisiana": 8, 
+    "Maine": 4, "Maryland": 10, "Massachusetts": 11, "Michigan": 16, "Minnesota": 10, "Mississippi": 6, "Missouri": 10, "Montana": 3, "Nebraska": 5, 
+    "Nevada": 6, "New Hampshire": 4, "New Jersey": 14, "New Mexico": 5, "New York": 29, "North Carolina": 15, "North Dakota": 3, "Ohio": 18, 
+    "Oklahoma": 7, "Oregon": 7, "Pennsylvania": 20, "Rhode Island": 4, "South Carolina": 9, "South Dakota": 3, "Tennessee": 11, "Texas": 38, 
+    "Utah": 6, "Vermont": 3, "Virginia": 13,"Washington": 12, "West Virginia": 5, "Wisconsin": 10, "Wyoming": 3
+}
+```
+
+!["'The Donald'"](media/trump-dance.gif "'The Donald'"){:data-caption="'The Donald'" width="350px"}
+
+Indien in een staat een bepaalde kandidaat wint, dan worden alle kiesmannen van die staat aan die kandidaat toegewezen. Indien Donald Trump in de staat Texas wint, dan krijgt hij alle 38 kiesmannen toegewezen.
+
+Stel dat je van alle staten telkens de percentages voor de verschillende kandidaten krijgt, bepaal dan het aantal kiesmannen dat elke kandidaat krijgt. Je krijgt de uitslag van de verkiezingen in een dictionary van de vorm `<staat>:<percentages>` waarbij `percentages` een lijst voorstelt met telkens de kandidaat en het behaalde percentage.
+
+Hoewel de focus in Amerika steeds op twee kandidaten ligt, komen er soms nog andere kandidaten op. Meestal is het percentage dat deze kandidaten bemachten wel vrij klein.
+
+## Gevraagd
+Schrijf een functie `president(uitslagen)` die gegeven dergelijke dictionary een nieuwe dictionary met het aantal kiesmannen per kandidaat bepaalt.
+
+Bestudeer grondig onderstaand voorbeeld.
+
+#### Voorbeelden
+
+```python
+>>> president({"Texas": ["Donald Trump", 56.3, "Kamela Harris", 42.4, "Jill Stein", 0.7, "Chase Oliver", 0.6],
+               "New Mexico": ["Donald Trump", 46.1, "Kamela Harris", 51.6, "Jill Stein", 0.5, "Robbert Kennedy", 1],
+               ...})
+{"Donald Trump": 295, 
+ "Kamela Harris": 243}
+```
+
+{: .callout.callout-info}
+>#### Tip
+> Gebruik de gegeven dictionary `staten`.
---
+++ Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/09 Amerikaanse verkiezingen/preparation/generator.py
@@ -0,0 +1,108 @@
+import os
+import importlib.util
+import random
+import ruamel.yaml
+import contextlib, io
+import math
+yaml = ruamel.yaml.YAML()
+
+
+# set fixed seed for generating test cases
+random.seed(12345678)
+
+# locate evaldir
+evaldir = os.path.join('..', 'evaluation')
+if not os.path.exists(evaldir):
+    os.makedirs(evaldir)
+
+# locate solutiondir
+solutiondir = os.path.join('..', 'solution')
+if not os.path.exists(solutiondir):
+    os.makedirs(solutiondir)
+
+def write_yaml( data:list ):
+    """ A function to write YAML file"""
+    with open(os.path.join('..', 'evaluation', 'tests.yaml'), 'w', encoding='utf-8') as f:
+        yaml.dump(data, f)
+
+module_name = 'solution'
+file_path = os.path.join(solutiondir, 'solution.nl.py')
+spec = importlib.util.spec_from_file_location(module_name, file_path)
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+
+# generate test data
+staten = {
+    "Alabama": 9, "Alaska": 3, "Arizona": 11, "Arkansas": 6, "California": 55, "Colorado": 9, "Connecticut": 7, "Delaware": 3, "District of Columbia": 3,
+    "Florida": 29, "Georgia": 16, "Hawaii": 4, "Idaho": 4, "Illinois": 20, "Indiana": 11, "Iowa": 6, "Kansas": 6, "Kentucky": 8, "Louisiana": 8, 
+    "Maine": 4, "Maryland": 10, "Massachusetts": 11, "Michigan": 16, "Minnesota": 10, "Mississippi": 6, "Missouri": 10, "Montana": 3, "Nebraska": 5, 
+    "Nevada": 6, "New Hampshire": 4, "New Jersey": 14, "New Mexico": 5, "New York": 29, "North Carolina": 15, "North Dakota": 3, "Ohio": 18, 
+    "Oklahoma": 7, "Oregon": 7, "Pennsylvania": 20, "Rhode Island": 4, "South Carolina": 9, "South Dakota": 3, "Tennessee": 11, "Texas": 38, 
+    "Utah": 6, "Vermont": 3, "Virginia": 13,"Washington": 12, "West Virginia": 5, "Wisconsin": 10, "Wyoming": 3
+}
+candidates = ["Robert Kennedy", "Jill Stein", "Chase Oliver"]
+
+ntests = 20
+
+cases = []
+
+while len(cases) < ntests:
+    case = {}
+    for staat in staten:
+        cand = random.sample(candidates, k = random.randint(1,3))
+        
+        s1 = round(random.uniform(44,56), 1)
+        s2 = round(random.uniform(44, 100-s1-1), 1)
+        rem = 100 - s1-s2
+        templist = [ random.randint(1,100) for _ in range(len(cand))]
+        remlist = [round(templist[i]/sum(templist)*rem, 1) for i in range(len(cand))]
+        
+        
+        list1 = ["Donald Trump", s1]
+        list2 = ["Kamela Harris", s2]
+        if random.randint(0,1) == 1:
+            list = list1 + list2
+        else:
+            list = list2 + list1
+        for c in range(len(cand)):
+            list.append(cand[c])
+            list.append(remlist[c])
+            
+        case[staat] = list
+    
+    if case not in cases:
+        cases.append(case)
+
+
+# generate unit tests for functions
+yamldata = []
+tabtitle = "Feedback"
+
+yamldata.append( {'tab': tabtitle, 'contexts': []})
+
+for i in range(len(cases)):
+    test = cases[i]
+    yamldata[0]['contexts'].append( {'testcases' : []})
+       
+    # generate test expression
+    expression_name = f"president({test})"
+    print(expression_name)
+    try:
+        outputF = io.StringIO()
+        with contextlib.redirect_stdout(outputF):
+            result = module.president(test)
+        stdout = outputF.getvalue() ##printed value
+
+        print(stdout)
+        print(result)
+        # setup for return expressions
+        testcase = { "expression": expression_name, "return" : result }
+        yamldata[0]['contexts'][i]["testcases"].append( testcase)
+    except Exception as e:
+        print(e)    
+
+
+write_yaml(yamldata)
+
+
+
