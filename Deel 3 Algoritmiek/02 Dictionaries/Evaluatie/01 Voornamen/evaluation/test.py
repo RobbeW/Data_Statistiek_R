54320fd1c2c11f21086e24950c734df45b2b6e36
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-11-15 09:22:11 +0100

    fixing custom check

--- Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/01 Voornamen/evaluation/test.py
+++ Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/01 Voornamen/evaluation/test.py
@@ -5,9 +5,10 @@ def check_function(context: ConvertedOracleContext) -> EvaluationResult:
     # actual as given by running the file
     if isinstance(context.actual, dict) and isinstance(context.expected, dict):
         value = set(context.expected.keys()) == set(context.actual.keys())
-        for key, voornamen in context.expected.items():
-            value &= set(voornamen) == set(context.actual[key])
-            value &= type(voornamen) == type(context.actual[key])
+        if value:
+            for key, voornamen in context.expected.items():
+                value &= set(voornamen) == set(context.actual[key])
+                value &= type(voornamen) == type(context.actual[key])
     else:
         value = False
 
