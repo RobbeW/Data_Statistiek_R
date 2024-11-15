8ec3264e7137b1aeaf71ca41097cb17ceaf35a7c
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-11-15 09:16:20 +0100

    fixing test

--- Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/01 Voornamen/evaluation/test.py
+++ Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/01 Voornamen/evaluation/test.py
@@ -3,10 +3,13 @@ from evaluation_utils import EvaluationResult, ConvertedOracleContext
 def check_function(context: ConvertedOracleContext) -> EvaluationResult:
     # expected as defined in the value field of the test suit
     # actual as given by running the file
-    value = set(context.expected.keys()) == set(context.actual.keys())
-    for key, voornamen in context.expected.items():
-        value &= set(voornamen) == set(context.actual[key])
-        value &= type(voornamen) == type(context.actual[key])
+    if isinstance(context.actual, dict):
+        value = set(context.expected.keys()) == set(context.actual.keys())
+        for key, voornamen in context.expected.items():
+            value &= set(voornamen) == set(context.actual[key])
+            value &= type(voornamen) == type(context.actual[key])
+    else:
+        value = False
 
     obj = EvaluationResult(result = value)
     return obj
