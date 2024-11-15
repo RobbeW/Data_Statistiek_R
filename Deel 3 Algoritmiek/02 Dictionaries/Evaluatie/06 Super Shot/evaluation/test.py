80914ad5fada10654e922ed04843069a1099452c
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-11-15 09:28:33 +0100

    fix custom check

--- Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/06 Super Shot/evaluation/test.py
+++ Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/06 Super Shot/evaluation/test.py
@@ -3,7 +3,10 @@ from evaluation_utils import EvaluationResult, ConvertedOracleContext
 def check_function(context: ConvertedOracleContext) -> EvaluationResult:
     # expected as defined in the value field of the test suit
     # actual as given by running the file
-    value = (sorted(context.expected) == sorted(context.actual))
-    value &= type(context.expected) == type(context.actual)
+    if isinstance(context.actual, list):
+        value = (sorted(context.expected) == sorted(context.actual))
+        value &= type(context.expected) == type(context.actual)
+    else:
+        value = False
     obj = EvaluationResult(result = value)
     return obj
