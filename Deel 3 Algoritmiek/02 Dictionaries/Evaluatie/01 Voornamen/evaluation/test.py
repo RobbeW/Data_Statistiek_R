fa35af41c37f41e8e1ae91ad5f07262a46ff35c7
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-11-15 09:20:14 +0100

    fixing check

--- Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/01 Voornamen/evaluation/test.py
+++ Deel 3 Algoritmiek/02 Dictionaries/Evaluatie/01 Voornamen/evaluation/test.py
@@ -3,7 +3,7 @@ from evaluation_utils import EvaluationResult, ConvertedOracleContext
 def check_function(context: ConvertedOracleContext) -> EvaluationResult:
     # expected as defined in the value field of the test suit
     # actual as given by running the file
-    if isinstance(context.actual, dict):
+    if isinstance(context.actual, dict) and isinstance(context.expected, dict):
         value = set(context.expected.keys()) == set(context.actual.keys())
         for key, voornamen in context.expected.items():
             value &= set(voornamen) == set(context.actual[key])
