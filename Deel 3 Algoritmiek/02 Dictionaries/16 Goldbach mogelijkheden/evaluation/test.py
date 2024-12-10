3e3e8a2e6c67decb03ee6586174fe762941ce2ac
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-12-10 22:17:24 +0100

    Testing custom formatting

---
+++ Deel 3 Algoritmiek/02 Dictionaries/16 Goldbach mogelijkheden/evaluation/test.py
@@ -0,0 +1,30 @@
+from evaluation_utils import EvaluationResult, ConvertedOracleContext
+
+def pretty_print_dict(dictio):
+    txt = "\{"
+    i = 1
+    for key, value in dictio:
+        i += 1
+        txt += f"{key} : {repr(value)}"
+        if i != len(dictio):
+            txt += ",\n"
+        txt += "\}"
+    return txt
+
+def check_function(context: ConvertedOracleContext) -> EvaluationResult:
+    # expected as defined in the value field of the test suit
+    # actual as given by running the file
+    
+    if isinstance(context.actual, dict):
+        value = context.expected == context.actual
+    else:
+        value = False
+    
+    readable_expected = pretty_print_dict(context.expected)
+    readable_actual = pretty_print_dict(context.actual)
+    
+    obj = EvaluationResult(
+        result = value,
+        readable_expected = readable_expected,
+        readable_actual = readable_actual)
+    return obj
