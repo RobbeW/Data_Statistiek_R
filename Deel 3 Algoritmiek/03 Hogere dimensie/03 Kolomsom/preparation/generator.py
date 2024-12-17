507f92abf38d6eb23c68b267609dcbc4962b3cf5
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-12-17 18:08:01 +0100

    Fix for formatting

--- Deel 3 Algoritmiek/03 Hogere dimensie/03 Kolomsom/preparation/generator.py
+++ Deel 3 Algoritmiek/03 Hogere dimensie/03 Kolomsom/preparation/generator.py
@@ -31,6 +31,37 @@ spec = importlib.util.spec_from_file_location(module_name, file_path)
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
 
+def find_longest(matrix):
+    longest = 0
+    for r in range(len(matrix)):
+        for c in range(len(matrix[0])):
+            el = str(matrix[r][c])
+            if len(el) > longest:
+                longest = len(el)
+    return longest
+
+def generate_expression(name, matrix):
+    # probably not the best method
+    dist = find_longest(matrix)
+    name_length = len(name)
+    
+    txt = f"{name}(["
+    for r in range(len(matrix)):
+        # insert leading spaces
+        if r > 0:
+            txt += f"{' ':>{name_length+2}}"
+        txt += "["
+        for c in range(len(matrix[0])):
+            el = matrix[r][c]
+            txt += f"{el:>{dist}}"
+            if c < len(matrix[0]) - 1:
+                txt += ", "
+        txt += "]"
+        if r < len(matrix) - 1:
+            txt += ",\n"
+    txt += "])"
+    return txt
+
 # generate test data
 ntests = 20
 cases = [ ]
@@ -56,6 +87,9 @@ for i in range(len(cases)):
        
     # generate test expression
     expression_name = f"kolomsom({test})"
+    description_name = generate_expression("kolomsom", test)
+    description = {"description": description_name, "format": "python"}
+    
 
     try:
         outputF = io.StringIO()
@@ -66,7 +100,9 @@ for i in range(len(cases)):
         print(stdout)
         print(result)
         # setup for return expressions
-        testcase = { "expression": expression_name, "return" : result }
+        testcase = { "expression": expression_name,
+                     "description": description,
+                     "return" : result }
         yamldata[0]['contexts'][i]["testcases"].append( testcase)
     except Exception as e:
         print(e)    
