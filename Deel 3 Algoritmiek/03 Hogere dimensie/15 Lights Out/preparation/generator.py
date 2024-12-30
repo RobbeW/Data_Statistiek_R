212b5b0407d4acbdd2a4707708ac385d9c3083f9
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-12-30 01:10:26 +0100

    removed unneeded function

--- Deel 3 Algoritmiek/03 Hogere dimensie/15 Lights Out/preparation/generator.py
+++ Deel 3 Algoritmiek/03 Hogere dimensie/15 Lights Out/preparation/generator.py
@@ -33,32 +33,6 @@ spec = importlib.util.spec_from_file_location(module_name, file_path)
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
 
-def oxo_check(rooster):
-    xwint = 0
-    owint = 0
-    for r in range(3):
-        if rooster[r] == ["X", "X", "X"]:
-            xwint +=1
-        elif rooster[r] == ["O", "O", "O"]:
-            owint += 1
-        if rooster[0][r] == rooster[1][r] == rooster[2][r] == "X":
-            xwint +=1
-        if rooster[0][r] == rooster[1][r] == rooster[2][r] == "O":
-            owint += 1
-        
-    if rooster[0][0] == rooster[1][1] == rooster[2][2] == "X":
-        xwint +=1
-    elif rooster[0][0] == rooster[1][1] == rooster[2][2] == "O":
-        owint +=1
-        
-    if rooster[2][0] == rooster[1][1] == rooster[0][2] == "X":
-        xwint +=1
-    elif rooster[2][0] == rooster[1][1] == rooster[0][2] == "O":
-        owint +=1
-           
-    return xwint > 0 or owint > 0
-
-
 def find_longest(matrix):
     longest = 0
     for r in range(len(matrix)):
