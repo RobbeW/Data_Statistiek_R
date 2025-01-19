52bebaaa544ed86399a7d8f42068970cb803b03e
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2025-01-19 22:11:24 +0100

    Fixing test?

--- Deel 3 Algoritmiek/03 Hogere dimensie/11 Hervorm/evaluation/test.py
+++ Deel 3 Algoritmiek/03 Hogere dimensie/11 Hervorm/evaluation/test.py
@@ -40,7 +40,7 @@ def check_function(context: ConvertedOracleContext) -> EvaluationResult:
 
 
     readable_expected = prettify_matrix_num(list(context.expected))
-    if isinstance(context.actual, list) and isinstance(context.actual[0], list):
+    if isinstance(context.actual, list) and isinstance(context.actual[0], list) and isinstance(context.actual[0][0], int):
         readable_actual = prettify_matrix_num(list(context.actual))
     else:
         readable_actual = context.actual
