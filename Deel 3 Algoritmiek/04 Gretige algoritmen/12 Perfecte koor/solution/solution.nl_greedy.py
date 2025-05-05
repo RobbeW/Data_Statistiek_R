011058142f2b7a68229fc209d3775634d8b3a4d1
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2025-05-05 10:02:48 +0200

    Better greedy

--- Deel 3 Algoritmiek/04 Gretige algoritmen/12 Perfecte koor/solution/solution.nl_greedy.py
+++ Deel 3 Algoritmiek/04 Gretige algoritmen/12 Perfecte koor/solution/solution.nl_greedy.py
@@ -8,25 +8,9 @@ def perfecte_koor(toonhoogtes):
     
     resultaat = 1
     i = 0
-    j = n - 1
-    while i < j:
-        links = toonhoogtes[i]
-        rechts = toonhoogtes[j]
-        if gem - links < rechts - gem:
-            resultaat += gem - links
-            toonhoogtes[i] += (gem - links)
-            toonhoogtes[j] -= (gem - links)
-            i += 1
-        elif gem - links > rechts - gem:
-            resultaat += (rechts - gem)
-            toonhoogtes[i] += rechts - gem
-            toonhoogtes[j] -= rechts - gem
-            j -= 1
-        else:
-            resultaat += rechts - gem
-            toonhoogtes[i] += rechts - gem
-            toonhoogtes[j] -= rechts - gem
-            j -= 1
-            i += 1        
+    while toonhoogtes[i] < gem:
+        resultaat += gem - toonhoogtes[i]
+        toonhoogtes[i] = gem
+        i += 1
     
     return resultaat
