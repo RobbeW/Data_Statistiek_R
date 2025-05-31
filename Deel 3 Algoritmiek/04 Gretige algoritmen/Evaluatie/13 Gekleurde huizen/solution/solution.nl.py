21ea9b9edba42e6ffd320984d5f5d418b148d8db
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2025-05-31 16:32:38 +0200

    fixing tests

--- Deel 3 Algoritmiek/04 Gretige algoritmen/Evaluatie/13 Gekleurde huizen/solution/solution.nl.py
+++ Deel 3 Algoritmiek/04 Gretige algoritmen/Evaluatie/13 Gekleurde huizen/solution/solution.nl.py
@@ -4,7 +4,7 @@ def maximale_afstand(huizen):
     i = 0
     while i < n and huizen[n - 1 - i] == huizen[0]:
         i += 1
-    maxiL = i
+    maxiL = n - 1 - i
     
     i = 0
     while i < n and huizen[i] == huizen[n - 1]:
