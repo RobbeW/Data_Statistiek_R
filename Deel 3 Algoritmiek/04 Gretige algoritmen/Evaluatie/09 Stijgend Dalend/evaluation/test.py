e2e92be4a40f8a6e2e9af96ef6d3f04a13b51e96
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2025-06-09 08:55:35 +0200

    json output problem?

--- Deel 3 Algoritmiek/04 Gretige algoritmen/Evaluatie/09 Stijgend Dalend/evaluation/test.py
+++ Deel 3 Algoritmiek/04 Gretige algoritmen/Evaluatie/09 Stijgend Dalend/evaluation/test.py
@@ -12,16 +12,18 @@ from evaluation_utils import EvaluationResult, ConvertedOracleContext, Message
 def check_function(context: ConvertedOracleContext) -> EvaluationResult:
     # expected as defined in the value field of the test suite
     # actual as given by running the file
-    if isinstance(context.actual, list) and isinstance(context.expected, list):
-        # does the actual list contain all numbers from 0 to n-1?
-        value = True
-        #for i in range(len(context.actual)):
-        #    value &= i in context.actual
-        #
-        ## check if DI strings match
-        #value = value and (make_di_str(context.actual) == make_di_str(context.expected))
-    else:
-        value = False
+    #if isinstance(context.actual, list) and isinstance(context.expected, list):
+    #    # does the actual list contain all numbers from 0 to n-1?
+    #    value = True
+    #    #for i in range(len(context.actual)):
+    #    #    value &= i in context.actual
+    #    #
+    #    ## check if DI strings match
+    #    #value = value and (make_di_str(context.actual) == make_di_str(context.expected))
+    #else:
+    #    value = False
+
+    value = True
 
     if value:
         obj = EvaluationResult(
