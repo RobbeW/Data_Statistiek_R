c82232ae09af49579f89cd7a1a315943718c952a
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2025-06-09 12:31:09 +0200

    Fixing tests

--- Deel 3 Algoritmiek/04 Gretige algoritmen/Evaluatie/09 Stijgend Dalend/evaluation/test.py
+++ Deel 3 Algoritmiek/04 Gretige algoritmen/Evaluatie/09 Stijgend Dalend/evaluation/test.py
@@ -1,27 +1,27 @@
 from evaluation_utils import EvaluationResult, ConvertedOracleContext, Message
 
-#def make_di_str(list):
-#    txt = ""
-#    for i in range(len(list)-1):
-#        if list[i] < list[i+1]:
-#            txt += "I"
-#        else:
-#            txt += "D"
-#    return txt
+def make_di_str(list):
+    txt = ""
+    for i in range(len(list)-1):
+        if list[i] < list[i+1]:
+            txt += "I"
+        else:
+            txt += "D"
+    return txt
 
 def check_function(context: ConvertedOracleContext) -> EvaluationResult:
     # expected as defined in the value field of the test suite
     # actual as given by running the file
-    #if isinstance(context.actual, list) and isinstance(context.expected, list):
-    #    # does the actual list contain all numbers from 0 to n-1?
-    #    value = True
-    #    #for i in range(len(context.actual)):
-    #    #    value &= i in context.actual
-    #    #
-    #    ## check if DI strings match
-    #    #value = value and (make_di_str(context.actual) == make_di_str(context.expected))
-    #else:
-    #    value = False
+    if isinstance(context.actual, list) and isinstance(context.expected, list):
+        # does the actual list contain all numbers from 0 to n-1?
+        value = True
+        for i in range(len(context.actual)):
+            value &= i in context.actual
+        
+        # check if DI strings match
+        value = value and (make_di_str(context.actual) == make_di_str(context.expected))
+    else:
+        value = False
 
     value = True
 
