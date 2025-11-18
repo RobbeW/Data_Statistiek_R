92a6b1ab05d5b8290a130129b16e55b3a9045b8b
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2025-11-18 19:10:51 +0100

    Update Feedback.R

--- Deel 1 Statistiek/04 Grafische weergaven/Z - Evaluatie/P+R Gent/evaluation/Feedback.R
+++ Deel 1 Statistiek/04 Grafische weergaven/Z - Evaluatie/P+R Gent/evaluation/Feedback.R
@@ -1,12 +1,20 @@
-# Importeert de data van Stad Gent
-data <- read.csv2("https://data.stad.gent/api/explore/v2.1/catalog/datasets/real-time-bezetting-pr-gent/exports/csv",
-                  sep = ";",
-                  colClasses = c("character", rep("NULL", 3),"numeric", rep("NULL", 4), rep("numeric", 2), rep("NULL", 7)) )
+# Importeert de data van Stad Gent (nieuwe dataset parkeergarages)
+data <- read.csv2(
+  "https://data.stad.gent/api/v2/catalog/datasets/bezetting-parkeergarages-real-time/exports/csv",
+  sep = ";",
+  dec = ","
+)
+
+# Hou alleen de relevante kolommen
+data <- data[, c("name", "isopennow", "totalcapacity", "availablecapacity")]
+
+# Zelfde namen als in je oorspronkelijke oefening
 colnames(data) <- c("naam", "nu_open", "aantal_plaatsen", "beschikbaar")
 data$nu_open <- data$nu_open == 1
 
-# Plot
-bezettingsgraad <- round((1-data$beschikbaar/data$aantal_plaatsen) * 100, 1)
+# Referentiewaarde voor de tests
+bezettingsgraad <- round((1 - data$beschikbaar / data$aantal_plaatsen) * 100, 1)
+
 
 context({
   testcaseAssert("De variabele bezettingsgraad bestaat.", function(env) {
