f73619bd1c0c8e8276256105ed1752f2e3593916
Exy Sabbe <exy.sabbe@sintlievenscollege.be>
2026-03-10 16:50:59 +0100

    Fix exercise solution

--- Deel 2 Python/03 Algoritmen uit de wiskunde/04 - Bisectiemethode/solution/solution.nl.py
+++ Deel 2 Python/03 Algoritmen uit de wiskunde/04 - Bisectiemethode/solution/solution.nl.py
@@ -1,11 +1,12 @@
-def bisectiemethode( a, b, f ):
+def bisectiemethode(a, b, f):
     for i in range(40):
-        y_a = f(a)
-        x_nieuw = (a + b) / 2
-        y_nieuw = f(x_nieuw)
-        if y_a*y_nieuw < 0:
-            b = x_nieuw
+        nieuw = (a + b) / 2
+        if f(a) * f(nieuw) < 0:
+            b = nieuw
+        elif f(a) * f(nieuw) > 0:
+            a = nieuw
         else:
-            a = x_nieuw
-        print(f"In iteratie {i+1} is de benadering: {round(x_nieuw, 15)}")
-    return (a+b)/2
+            print("In iteratie", i + 1, "is de benadering:", round(nieuw, 15))
+            return nieuw
+        print("In iteratie", i + 1, "is de benadering:", round(nieuw, 15))
+    return nieuw
